5e79d97be1d269453e3b597f1288f0d5ddb866f1
charafarmaki <97169678+charafarmaki@users.noreply.github.com>
2024-10-16 14:12:15 +0300

    Create Constants.h

---
+++ Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MUSIC_ENABLED true
+#define MUSIC_EFFECTS true
+#define ENABLE_BARRELS true
+#define BORDER_MARIO false 
+#define BORDER_LADDERS false
+#define SOUND_VOLUME 0.3f
