a537c5fa12e522a2a5b6930321d3c69afe7dfcb4
charafarmaki <97169678+charafarmaki@users.noreply.github.com>
2024-10-16 14:01:57 +0300

    Create Enemy.cpp

---
+++ Enemy.cpp
@@ -0,0 +1,5 @@
+#include "Enemy.h" 
+
+Enemy::Enemy(float x, float y, float width, float height) : GameObject(x, y, width, height) { // Enemy constructor initializing GameObject with given parameters
+
+}
